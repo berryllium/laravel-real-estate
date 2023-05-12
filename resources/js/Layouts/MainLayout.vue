c139b711d66d5c0f663ca6bcb3b4efbd04e9b78c
berryllium <gorkundp@yandex.ru>
2023-05-12 22:10:29 +0200

    init

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <Link href="/">Main Page</Link>&nbsp;
+  <Link href="/hello">Show Page</Link>
+  <div>This page with time {{ timer }}</div>
+  <slot />
+</template>
+
+<script setup>
+import {Link} from '@inertiajs/vue3'
+import {ref} from 'vue'
+
+const timer = ref(0)
+setInterval(() => timer.value++, 1000)
+</script>
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Index</div>
+  <div>
+    The message is: {{ message }}
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  'message': String,
+})
+</script>
