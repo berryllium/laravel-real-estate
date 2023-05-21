39e65a56c41964efc81565b152226e13ec0a53ac
gorkun <dmitry.gorkun@oodji.com>
2023-05-21 23:42:04 +0200

    login form

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function create() {
+        return inertia('Auth/Login');
+    }
+
+    public function store() {
+
+    }
+
+    public function destroy() {
+
+    }
+}
