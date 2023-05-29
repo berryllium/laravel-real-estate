fd5d2ccc837ea0f5efc78a2df99c63c866b48baa
gorkun <dmitry.gorkun@oodji.com>
2023-05-29 20:53:32 +0200

    pagination

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -23,7 +23,7 @@ class ListingController extends Controller
         return inertia(
             'Listing/Index',
             [
-                'listings' => Listing::all()
+                'listings' => Listing::orderByDesc('created_at')->paginate(5)
             ]
         );
     }
