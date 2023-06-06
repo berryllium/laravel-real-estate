93b16bcf56514189ed7d332aa0a4bf31fd11e0b1
berryllium <gorkundp@yandex.ru>
2023-06-06 21:52:18 +0200

    fix reload page with filters

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -33,6 +33,7 @@
 import { reactive, watch, computed } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 import { debounce } from 'lodash'
+import { router } from '@inertiajs/core';
 
 const sortLabels = {
   created_at: [
@@ -69,7 +70,7 @@ const filterForm = reactive({
   order: props.filters.order ?? 'desc',
 })
 watch(
-  filterForm, debounce(() => Inertia.get(
+  filterForm, debounce(() => router.get(
     route('realtor.listing.index'),
     filterForm,
     { preserveState: true, preserveScroll: true },
